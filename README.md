3f2735f4ef74239cb9d1ec0ea829a2e8a7bb005e
Yassine Yaakoubi <yacoubiiyacine@gmail.com>
2020-06-02 02:16:29 -0400

    First commit (1)

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Struct-CKN
+# Our Paper Title
+
+This repository is the official implementation of Structured Convolutional Kernel Networks for Airline Crew Scheduling.
+
+## Requirements
+
+To install requirements:
+
+```setup
+pip install --upgrade -r requirements.txt
+```
+
+
+Use python2 or python<=3.6.10 to intall pystruct.
+If you use python3.7, see [this link](https://stackoverflow.com/questions/24764549/upgrade-python-packages-from-requirements-txt-using-pip-command) for directions on how to install pystruct:
+
+
+## Training
+
+
+To train the model(s) in the paper, run this command:
+
+```train
+python main.py
+```
+
+To use the block-coordinate Frank-Wolfe predictor, run this command:
+
+```train
+python main.py --predictor fw
+```
+
+
+For a complete list of arguments, run this command:
+```train
+python main.py --help
+```
+
+## Evaluation
+
+To evaluate our model on the OCR dataset, run:
+
+```eval
+python eval.py
+```
+
+## Pre-trained Models
+
+The pretrained model is in the the folder pretrained.
+
+## Results
+
+Our model achieves the following performance on :
+
+### [OCR dataset]
+
+| Model name         | Test error    |
+| ------------------ |-------------- |
+| Struct-CKN (SDCA)  |     3.40%     |
+| Struct-CKN (BCFW)  |     3.42%     |
+
+
+
+
+
---
+++ script_supervised_SVM.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+echo Running on $HOSTNAME
+echo with old_init_W
+echo with fit
+echo with clf-fit-init = True each epoch
+echo n_filters 200
+benchmark=0
+predictor=sdca
+numberEpochsUnsupCKN=100
+numberEpochsFW=10
+numberEpochsCKN=100
+regParBCFW=1.0
+##regParSDCA=
+gpu=0
+##scaler=3
+lr=0.1
+#npass= 1 10
+size_patch=5
+## zero_prob=0 or 0.0001
+non_uniformity=0.8
+sampling_scheme=gap
+line_search=golden
+init_previous_step_size=0
+use_warm_start=1
+echo benchmark $benchmark
+echo predictor $predictor
+echo numberEpochsUnsupCKN $numberEpochsUnsupCKN
+echo numberEpochsFW $numberEpochsFW
+echo numberEpochsCKN $numberEpochsCKN
+echo regParBCFW $regParBCFW
+echo regParSDCA $1
+echo gpu $gpu
+echo scaler $2
+echo lr $lr
+echo npass $3
+echo size_patch $size_patch
+echo zero_prob $4
+echo non_uniformity $non_uniformity
+echo sampling_scheme $sampling_scheme
+echo line_search $line_search
+echo init_previous_step_size $init_previous_step_size
+echo use_warm_start $use_warm_start
+cd /network/home/yaakouby/SCKN_August_2019/Merged_Code/
+python3.6 supervised_OCR.py --numberEpochsFW $numberEpochsFW --numberEpochsCKN $numberEpochsCKN --regParBCFW $regParBCFW --gpu $gpu --scaler $2 --lr $lr --npass $3 --size-patch $size_patch --zero-prob $4 --non-uniformity $non_uniformity --sampling-scheme $sampling_scheme --line-search $line_search --init-previous-step-size $init_previous_step_size --use-warm-start $use_warm_start --predictor $predictor --numberEpochsUnsupCKN $numberEpochsUnsupCKN --benchmark $benchmark --regParSDCA $1
